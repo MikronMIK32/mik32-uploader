6af05b8808ca1748b877cd747f01dfe28d72a525
Sergey Shchelkanov <shchelkanov-s@yandex.ru>
2023-05-30 16:07:10 +0300

    revert to working state

--- connectopenocd-m-link.bat
+++ connectopenocd-m-link.bat
@@ -1,3 +1,3 @@
 @echo off
-openocd\bin\openocd.exe -s openocd/share/openocd/scripts -f interface/ftdi/m-link.cfg -f target/mcu32.cfg
+openocd\bin\openocd.exe -s openocd/share/openocd/scripts -f interface/ftdi/m-link.cfg -f target/mik32.cfg
 timeout /t 300
